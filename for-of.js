2bb8823d10bb778b8b34060888e36d25c927c6df
Rana Bhai <sarifulislam536@gmail.com>
2026-01-29 00:55:54 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,15 @@
+/* const num1 = [11,12,34,22,34,24];
+console.log(num1);
+
+for(const num of num1){
+  console.log(num);
+  
+} */
+
+  const num1 = [11,12,34,22,34,24];
+
+  for (const num of num1 ){
+console.log(num);
+console.log("i have ");
+
+  }
