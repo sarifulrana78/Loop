165249e6574e190175f3b0c9ac16324fffa79d4c
Rana Bhai <sarifulislam536@gmail.com>
2026-01-30 03:49:20 +0600

    problem solving of for loop

--- for.js
+++ for.js
@@ -1,6 +1,13 @@
-for (let num=0; num<=10; num++){
+/* for (let num=0; num<=10; num++){
     console.log(num)
     if(num%2===0){
         console.log("even",num)
     }
-}
+} */
+ let sum=0;
+    for (let i=11;i <=20;i++){
+        console.log(i);
+       
+        sum=sum+i;
+        console.log("sum",sum)
+    }
