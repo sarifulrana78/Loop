daf6901df47ffb8ea54b2a737ea53da21189a671
Rana Bhai <sarifulislam536@gmail.com>
2026-01-30 03:17:38 +0600

    count total num from1 to 10

--- range.js
+++ range.js
@@ -1,5 +1,20 @@
-let num= 1;
-while(num<10){
+/* let num= 1;
+while(num<=10){
     console.log(num);
     num++;
+} */
+
+/* let num=50;
+while (num<=100){
+console.log(num);
+num++;
+} */
+
+let num1=1;
+let sum=0;
+while(num1<=10){
+    console.log(num1);
+    sum=sum+num1;
+    console.log("total",sum)
+    num1++;
 }
